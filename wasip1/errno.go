d0f66da6ad25d97ae1291c5d8979624042d492d2
Gaukas Wang <i@gaukas.wang>
2024-01-12 18:22:30 -0700

    ⚠️ deprecated: Errno as an error
    WATM implementation should stick to using syscall.Errno and WATER Runtime-returned error code, but not an additional Errno. Therefore, we remove Errno type by making it unexported.

--- wasip1/errno.go
+++ wasip1/errno.go
@@ -5,53 +5,43 @@ import (
 	"syscall"
 )
 
-// Errno is just a copy of syscall.Errno from the Go standard library.
+// errno is just a copy of syscall.Errno from the Go standard library.
 //
 // The values are defined in syscall/tables_wasip1.go.
-type Errno int32
+type errno syscall.Errno
 
-// FromSyscallErrno converts a syscall.Errno into a Errno on wasip1.
-func FromSyscallErrno(errno syscall.Errno) Errno {
-	if foundErrno, ok := mapSyscallErrno[errno]; ok {
-		return foundErrno
-	}
-	panic(fmt.Sprintf("Errno %v(%d): %v(%d)", errno, errno, ENOSYS, ENOSYS))
-}
-
-func (e Errno) ToSyscallErrno() syscall.Errno {
-	if foundSyscallErrno, ok := mapErrnoSyscall[e]; ok {
-		return foundSyscallErrno
-	}
-	panic(fmt.Sprintf("Errno %v(%d): %v(%d)", e, e, syscall.ENOSYS, syscall.ENOSYS))
-}
-
-// Error returns the string representation of the error number.
-func (e Errno) Error() string {
-	if e == 0 {
-		return "ESUCCESS"
-	}
-
-	if e < 0 || e >= Errno(len(errorstr)) {
-		return fmt.Sprintf("unknown WATERErrno %d", e)
-	}
-
-	return errorstr[-e]
-}
-
-// WATERErrCode is defined as the negative value of Errno.
-func FromWATERErrCode(errorCode int32) (n int32, err error) {
-	if errorCode >= 0 {
-		n = errorCode // so when error code is 0, it will return 0, nil
-	} else if _, ok := mapErrnoSyscall[Errno(-errorCode)]; ok {
-		// if the negative of the error code is a valid Errno, then it is a valid WATERErrno.
-		err = Errno(-errorCode)
+// DecodeWATERError converts a error code returned by WATER API
+// into a syscall.Errno or a higher-level error in Go.
+//
+// It automatically detects whether the error code is a WATER error
+// or a success code (positive). In case of a success code, it
+// returns the code itself and a nil error.
+func DecodeWATERError(errorCode int32) (n int32, err error) {
+	if errorCode > 0 {
+		n = errorCode // such that when error code is 0, it will return 0, nil
 	} else {
-		// otherwise, it is an unknown error code.
-		err = Errno(ENOSYS)
+		errorCode = -errorCode // flip the sign
+		if errno, ok := mapErrno2Syscall[errno(errorCode)]; ok {
+			// if the negative of the error code is a valid Errno, then it is a valid WATERErrno.
+			err = errno
+
+			// TODO: convert some special error codes to higher-level errors.
+		} else {
+			// otherwise, it is an unknown error code.
+			err = fmt.Errorf("unknown WATERErrno %d", errorCode)
+		}
 	}
 	return
 }
 
-func (e Errno) WATER() int32 {
-	return int32(-e)
+func EncodeWATERError(errno syscall.Errno) int32 {
+	// first find the corresponding Errno (there might
+	// be missing Errno in the map, which means they
+	// are not supported)
+	if foundErrno, ok := mapSyscall2Errno[errno]; ok {
+		// then convert it to the negative value of itself
+		return -int32(foundErrno)
+	}
+	// if the errno is not found, then it is an unknown error
+	return -int32(syscall.ENOSYS)
 }
--- wasip1/tables.go
+++ wasip1/tables.go
@@ -4,83 +4,86 @@ import (
 	"syscall"
 )
 
+// errno is just a copy of syscall.Errno from the Go standard library.
+//
+// WATER error code is defined as the negative value of errno.
 const (
-	E2BIG           Errno = 1
-	EACCES          Errno = 2
-	EADDRINUSE      Errno = 3
-	EADDRNOTAVAIL   Errno = 4
-	EAFNOSUPPORT    Errno = 5
-	EAGAIN          Errno = 6
-	EALREADY        Errno = 7
-	EBADF           Errno = 8
-	EBADMSG         Errno = 9
-	EBUSY           Errno = 10
-	ECANCELED       Errno = 11
-	ECHILD          Errno = 12
-	ECONNABORTED    Errno = 13
-	ECONNREFUSED    Errno = 14
-	ECONNRESET      Errno = 15
-	EDEADLK         Errno = 16
-	EDESTADDRREQ    Errno = 17
-	EDOM            Errno = 18
-	EDQUOT          Errno = 19
-	EEXIST          Errno = 20
-	EFAULT          Errno = 21
-	EFBIG           Errno = 22
-	EHOSTUNREACH    Errno = 23
-	EIDRM           Errno = 24
-	EILSEQ          Errno = 25
-	EINPROGRESS     Errno = 26
-	EINTR           Errno = 27
-	EINVAL          Errno = 28
-	EIO             Errno = 29
-	EISCONN         Errno = 30
-	EISDIR          Errno = 31
-	ELOOP           Errno = 32
-	EMFILE          Errno = 33
-	EMLINK          Errno = 34
-	EMSGSIZE        Errno = 35
-	EMULTIHOP       Errno = 36
-	ENAMETOOLONG    Errno = 37
-	ENETDOWN        Errno = 38
-	ENETRESET       Errno = 39
-	ENETUNREACH     Errno = 40
-	ENFILE          Errno = 41
-	ENOBUFS         Errno = 42
-	ENODEV          Errno = 43
-	ENOENT          Errno = 44
-	ENOEXEC         Errno = 45
-	ENOLCK          Errno = 46
-	ENOLINK         Errno = 47
-	ENOMEM          Errno = 48
-	ENOMSG          Errno = 49
-	ENOPROTOOPT     Errno = 50
-	ENOSPC          Errno = 51
-	ENOSYS          Errno = 52
-	ENOTCONN        Errno = 53
-	ENOTDIR         Errno = 54
-	ENOTEMPTY       Errno = 55
-	ENOTRECOVERABLE Errno = 56
-	ENOTSOCK        Errno = 57
-	ENOTSUP         Errno = 58
-	ENOTTY          Errno = 59
-	ENXIO           Errno = 60
-	EOVERFLOW       Errno = 61
-	EOWNERDEAD      Errno = 62
-	EPERM           Errno = 63
-	EPIPE           Errno = 64
-	EPROTO          Errno = 65
-	EPROTONOSUPPORT Errno = 66
-	EPROTOTYPE      Errno = 67
-	ERANGE          Errno = 68
-	EROFS           Errno = 69
-	ESPIPE          Errno = 70
-	ESRCH           Errno = 71
-	ESTALE          Errno = 72
-	ETIMEDOUT       Errno = 73
-	ETXTBSY         Errno = 74
-	EXDEV           Errno = 75
-	ENOTCAPABLE     Errno = 76
+	E2BIG           errno = 1
+	EACCES          errno = 2
+	EADDRINUSE      errno = 3
+	EADDRNOTAVAIL   errno = 4
+	EAFNOSUPPORT    errno = 5
+	EAGAIN          errno = 6
+	EALREADY        errno = 7
+	EBADF           errno = 8
+	EBADMSG         errno = 9
+	EBUSY           errno = 10
+	ECANCELED       errno = 11
+	ECHILD          errno = 12
+	ECONNABORTED    errno = 13
+	ECONNREFUSED    errno = 14
+	ECONNRESET      errno = 15
+	EDEADLK         errno = 16
+	EDESTADDRREQ    errno = 17
+	EDOM            errno = 18
+	EDQUOT          errno = 19
+	EEXIST          errno = 20
+	EFAULT          errno = 21
+	EFBIG           errno = 22
+	EHOSTUNREACH    errno = 23
+	EIDRM           errno = 24
+	EILSEQ          errno = 25
+	EINPROGRESS     errno = 26
+	EINTR           errno = 27
+	EINVAL          errno = 28
+	EIO             errno = 29
+	EISCONN         errno = 30
+	EISDIR          errno = 31
+	ELOOP           errno = 32
+	EMFILE          errno = 33
+	EMLINK          errno = 34
+	EMSGSIZE        errno = 35
+	EMULTIHOP       errno = 36
+	ENAMETOOLONG    errno = 37
+	ENETDOWN        errno = 38
+	ENETRESET       errno = 39
+	ENETUNREACH     errno = 40
+	ENFILE          errno = 41
+	ENOBUFS         errno = 42
+	ENODEV          errno = 43
+	ENOENT          errno = 44
+	ENOEXEC         errno = 45
+	ENOLCK          errno = 46
+	ENOLINK         errno = 47
+	ENOMEM          errno = 48
+	ENOMSG          errno = 49
+	ENOPROTOOPT     errno = 50
+	ENOSPC          errno = 51
+	ENOSYS          errno = 52
+	ENOTCONN        errno = 53
+	ENOTDIR         errno = 54
+	ENOTEMPTY       errno = 55
+	ENOTRECOVERABLE errno = 56
+	ENOTSOCK        errno = 57
+	ENOTSUP         errno = 58
+	ENOTTY          errno = 59
+	ENXIO           errno = 60
+	EOVERFLOW       errno = 61
+	EOWNERDEAD      errno = 62
+	EPERM           errno = 63
+	EPIPE           errno = 64
+	EPROTO          errno = 65
+	EPROTONOSUPPORT errno = 66
+	EPROTOTYPE      errno = 67
+	ERANGE          errno = 68
+	EROFS           errno = 69
+	ESPIPE          errno = 70
+	ESRCH           errno = 71
+	ESTALE          errno = 72
+	ETIMEDOUT       errno = 73
+	ETXTBSY         errno = 74
+	EXDEV           errno = 75
+	ENOTCAPABLE     errno = 76
 	// needed by src/net/error_unix_test.go
 	EOPNOTSUPP = ENOTSUP
 )
@@ -165,7 +168,7 @@ var errorstr = [...]string{
 	ENOTCAPABLE:     "Capabilities insufficient",
 }
 
-var mapSyscallErrno = map[syscall.Errno]Errno{
+var mapSyscall2Errno = map[syscall.Errno]errno{
 	syscall.E2BIG:           E2BIG,
 	syscall.EACCES:          EACCES,
 	syscall.EADDRINUSE:      EADDRINUSE,
@@ -243,7 +246,7 @@ var mapSyscallErrno = map[syscall.Errno]Errno{
 	syscall.EXDEV:           EXDEV,
 }
 
-var mapErrnoSyscall = map[Errno]syscall.Errno{
+var mapErrno2Syscall = map[errno]syscall.Errno{
 	E2BIG:           syscall.E2BIG,
 	EACCES:          syscall.EACCES,
 	EADDRINUSE:      syscall.EADDRINUSE,
